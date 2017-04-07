520a426e19acc16d0e93d9fe8931db936eb62431
Guenter Bailey <office@bailey-solution.com>
2017-04-07 22:51:29 +0200

    startscript Daemon betrieb entfernt, da es schon im Dockerfile drinnen steht

--- Admidio/docker_build.sh
+++ Admidio/docker_build.sh
@@ -6,4 +6,4 @@ echo "starting Build"
 docker build -t $NAME:$VERSION .
 echo "build finished"
 echo "you can now starting the docker image with following Command"
-echo "docker run -d -it --name $NAME -p 8080:80 -v <local docker volume>:/var/www/html/admidio/adm_my_files $NAME:$VERSION"
+echo "docker run -it --restart always --name $NAME -p 8080:80 -v <local docker volume>:/var/www/admidio/adm_my_files $NAME:$VERSION"
